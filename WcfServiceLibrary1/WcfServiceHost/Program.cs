5e8ee0d3de7bd6e8d482bafd00cf57ee78d497ad
Mason-Moralles <zapevalovg892@gmail.com>
2025-11-23 19:22:16 +0300

    main done_1_cp

---
+++ WcfServiceLibrary1/WcfServiceHost/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ServiceModel;
+using WcfServiceLibrary1; // наш сервис из библиотеки
+
+namespace WcfServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Базовый адрес сервиса (можно заменить на свой порт/путь)
+            Uri baseAddress = new Uri("http://localhost:8733/Design_Time_Addresses/KT1/Service1/");
+
+            using (ServiceHost host = new ServiceHost(typeof(Service1), baseAddress))
+            {
+                try
+                {
+                    host.Open();
+                    Console.WriteLine("Сервис запущен.");
+                    Console.WriteLine("Адрес: " + baseAddress);
+                    Console.WriteLine("Нажмите Enter для остановки сервиса...");
+                    Console.ReadLine();
+                    host.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Ошибка при запуске сервиса: " + ex.Message);
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ WcfServiceLibrary1/WcfServiceHost/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("WcfServiceHost")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WcfServiceHost")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c66193cd-527b-4e29-a694-226e4863557d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
